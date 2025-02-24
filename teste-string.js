19bb663bdeccfc9792565c381e17b06a4a059574
Helcio Padilha <hspjunior@gmail.com>
2025-02-24 20:34:04 -0300

    Estamos aprendendo mais

--- teste-string.js
+++ teste-string.js
@@ -1,1 +1,7 @@
 const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'O autor comentou sobre as "boas práticas de programação"';
+console.log(citacao)
