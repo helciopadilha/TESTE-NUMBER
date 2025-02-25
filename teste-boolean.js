6b93f453057851252476578f900913354acbfc6f
Helcio Padilha <hspjunior@gmail.com>
2025-02-24 21:00:12 -0300

    Aqui tem booleno

---
+++ teste-boolean.js
@@ -0,0 +1,14 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero)
+
+const terceiroNumero = 5;
+const quartoNumero = 5;
+
+console.log(terceiroNumero === quartoNumero)
+
+const texto1 = "Colegio";
+const texto2 = "Dirce";
+
+console.log(texto1 === texto2)
